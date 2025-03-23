a9ede130d8eded0eb29f374dd8ec60df7bb6264c
unknown <gharish170@gmail.com>
2025-03-23 15:48:49 +0530

    dynaamic routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 const express= require('express')
 const app= express()
-   app.get("/",(req,res)=>{
-   res.send("Hello Iam Harish From Express")
-   })
-   app.get("/test",(req,res)=>{
-    res.send("Hello Iam Harish From Express test")
-   })
-   app.get("/hello",(req,res)=>{
-    res.send("Hello Iam Harish From Express")
-   })
-  
+
+app.get("/users/:regno/:name/:password",(req,res)=>{
+    console.log("The req are",req.params)
+    res.send("data got")
+})
+app.get("/users+",(req,res)=>{
+    res.send("Fetched data successfully")
+})
+
+app.get("/use?r(s)*",(req,res)=>{
+    res.send("Fetched data successfully")
+})
+app.post("/user",(req,res)=>{
+    res.send({"FirstName":"Harish",LastName:"Gotham"})
+})
+app.delete("/user",(req,res)=>{
+    res.send("Deleted successfully")
+})
+app.patch("/user",(req,res)=>{
+    res.send("Updated successfully")
+})
+app.use("/user",(req,res)=>{
+res.send("Hello Iam Harish From Express")
+})
 
 app.listen(3500,()=>{
     console.log("server is listening on the port number 3500....")
